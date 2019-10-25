13ff4368940b1bc1858e9895501839be4ecf2ab0
Lee Pedersen <leepedersenr@gmail.com>
2019-10-25 11:03:22 -0700

    edit if/else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     var q1 = parseInt($("input#question1").val());
+    // try and parse written numbers as ints
     var q2 = parseInt($("select#question2").val());
     var q3 = parseInt($("select#question3").val());
     var q4 = parseInt($("select#question4").val());
     var q5 = $("input#question5").val();
+    var q5 = q5.toLowerCase();
+    alert(q5);
 
     if (!q1 || !q2 || !q3 || !q4 || !q5) {
       alert("Please answer all questions");
       // make error message for if q1 is not an int
+      // make sure results do not show up in this case
+    } else if (q2 + q3 + q4 < 6 && q1 < 10) {
+      var answer = " python";
     } else if (q2 + q3 + q4 < 6) {
-      alert("python");
-      var answer = "You should learn python";
-    } else if (q2 + q3 + q4 < 9) {
-      alert("java")
-      var answer = "You should learn java";
+      var answer = " c++";
+    } else if (q2 + q3 + q4 < 9 && q5.startsWith("y")) {
+      var answer = " java";
     } else if (q2 + q3 + q4 >= 9) {
-      alert("ruby")
-      var answer = "You should learn ruby";
+      var answer = " ruby";
     } else {
-      var answer = "Congratulations, you did something real weird and I don't know what. Go learn all the languages at once."
+      var answer = "... congratulations, you did something real weird and I don't know what. Go learn all the languages at once."
       // link to hacking site
     }
+    //take time to come up with answer
+    $("#results").show();
+    $("#answer").text(answer);
     event.preventDefault();
   });
 });
