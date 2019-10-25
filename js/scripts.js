ee05bbaa0e0e2c956d9f5804b07de73d7dd5630f
Lee Pedersen <leepedersenr@gmail.com>
2019-10-25 10:27:56 -0700

    add more else ifs to make scripts more functional

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,22 @@ $(document).ready(function() {
     var q3 = parseInt($("select#question3").val());
     var q4 = parseInt($("select#question4").val());
     var q5 = $("input#question5").val();
-    alert(q5);
-    alert(q4);
-    alert(q3);
-    alert(q2);
-    alert(q1);
 
     if (!q1 || !q2 || !q3 || !q4 || !q5) {
       alert("Please answer all questions");
+      // make error message for if q1 is not an int
     } else if (q2 + q3 + q4 < 6) {
-      alert("holy shit it works");
+      alert("python");
       var answer = "You should learn python";
+    } else if (q2 + q3 + q4 < 9) {
+      alert("java")
+      var answer = "You should learn java";
+    } else if (q2 + q3 + q4 >= 9) {
+      alert("ruby")
+      var answer = "You should learn ruby";
     } else {
-      alert("it sorta works");
+      var answer = "Congratulations, you did something real weird and I don't know what. Go learn all the languages at once."
+      // link to hacking site
     }
     event.preventDefault();
   });
